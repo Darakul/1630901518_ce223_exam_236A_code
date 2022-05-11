995d3bd129af05f7a07658539c278466cd997206
Darakul <98445421+Darakul@users.noreply.github.com>
2022-05-11 21:49:37 +0700

    Add files via upload

---
+++ nodejs_sql.js
@@ -0,0 +1,46 @@
+//Open Call Express 
+const express = require('express')
+const bodyParser = require('body-parser')
+ 
+const mysql = require('mysql')
+ 
+const app = express()
+const port = process.env.PORT || 5000;
+ 
+app.use(bodyParser.json())
+ 
+//MySQL Connect phpMyAdmin
+const pool = mysql.createPool({
+    connectionLimit : 10,
+    connectionTimeout : 20,
+    host : 'localhost',
+    user : 'root',
+    password : '',
+    database : 'lottery' 
+})
+ 
+
+app.get('',(req, res) => {
+ 
+    pool.getConnection((err, connection) => {  
+        if(err) throw err
+        console.log("connected id : ?" ,connection.threadId) 
+         
+        connection.query('SELECT * FROM tbl_lottery', (err, rows) => { 
+            connection.release();
+            if(!err){ 
+                //console.log(rows)
+                //res.json(rows)
+                res.send(rows)
+            } else {
+                console.log(err)
+            }
+         }) 
+    })
+})
+ 
+
+
+app.listen(port, () => 
+    console.log("listen on port : ?", port)
+    )
